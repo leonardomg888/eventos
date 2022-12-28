c4ed9b9511d275d246579c26e4e35a2ebabce649
leonardomg888 <leonardomg888@gmail.com>
2022-12-28 02:39:24 +0100

    listo

---
+++ public/js/moviesAdd.js
@@ -0,0 +1,62 @@
+window.onload = function(){
+    let titulo = document.querySelector('.moviesAddTitulo')
+    let formulario = document.querySelector('#formulario');
+    let article = document.querySelector('article');
+    titulo.innerHTML = 'AGREGAR PELÍCULA';
+    titulo.classList.add('titulo');
+    article.classList.add('fondoTransparente');
+    formulario.classList.add('fondoCRUD');
+
+titulo.addEventListener("mouseover",()=>{
+    titulo.classList.toggle("botonBorrar")
+})
+titulo.addEventListener("mouseout",()=>{
+    titulo.classList.toggle("botonBorrar")
+})
+
+/*----------------- */
+
+let inputTitulo =document.getElementById("titulo");
+let estadoSecreto = 0;
+
+inputTitulo.addEventListener("keypress", (e) => {
+    switch(estadoSecreto) {
+        case 0:
+            e.key === "s" ? ++estadoSecreto : estadoSecreto = 0;
+            console.log(estadoSecreto)
+        break;    
+        case 1:
+            e.key === "e" ? ++estadoSecreto : estadoSecreto = 0;
+            console.log(estadoSecreto)
+        break;    
+        case 2:
+            e.key === "c" ? ++estadoSecreto : estadoSecreto = 0;
+            console.log(estadoSecreto)
+        break;    
+        case 3:
+            e.key === "r" ? ++estadoSecreto : estadoSecreto = 0;
+            console.log(estadoSecreto)
+        break;    
+        case 4:
+            e.key === "e" ? ++estadoSecreto : estadoSecreto = 0;
+            console.log(estadoSecreto)
+        break;    
+        case 5:
+            e.key === "t" ? ++estadoSecreto : estadoSecreto = 0;
+            console.log(estadoSecreto)
+        break;
+        case 6:
+            e.key === "o" ? ++estadoSecreto : estadoSecreto = 0;
+            console.log(estadoSecreto)
+            estadoSecreto === 7 && alert("SECRETO_MAGICO")
+            estadoSecreto = 0;
+        break;
+        default:
+            estadoSecreto= 0;
+        break;    
+        
+    }
+
+})
+
+}
